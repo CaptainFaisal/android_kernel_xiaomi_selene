LineageOS/android_kernel_xiaomi_sdm845

touchscreen: nt36xxx: Split MP_CTRLRAM to separate Kconfig
We don't need this anyway

Change-Id: Ied25af3d33394214b3cf992fde48615bff53d120
Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>

drivers/input/touchscreen/nt36xxx/nt36xxx.h
@@ -79,7 +79,11 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 //---Customerized func.---
 #define NVT_TOUCH_PROC 1
 #define NVT_TOUCH_EXT_PROC 1
+#ifdef CONFIG_TOUCHSCREEN_NT36XXX_MP_CTRLRAM
 #define NVT_TOUCH_MP 1
+#else
+#define NVT_TOUCH_MP 0
+#endif
 #define MT_PROTOCOL_B 1
 #define WAKEUP_GESTURE 1
 #if WAKEUP_GESTURE

CaptainFaisal/android_kernel_xiaomi_selene

drivers/input/touchscreen/mediatek/NT36672C/nt36xxx.h
@@ -106,7 +106,11 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 //---Customerized func.---
 #define NVT_TOUCH_PROC 1
 #define NVT_TOUCH_EXT_PROC 1
+#ifdef CONFIG_TOUCHSCREEN_NT36672C_MP_CTRLRAM
 #define NVT_TOUCH_MP 1
+#else
+#define NVT_TOUCH_MP 0
+#endif
 #define MT_PROTOCOL_B 1
 #define WAKEUP_GESTURE 1
 #define TP_SELFTEST 1